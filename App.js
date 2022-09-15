ea2c1bafafb8d7f9e1208557b7070081e1939658
Manav Bhanot <manavb@amazon.com>
2022-09-15 13:20:53 -0700

    Initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi. I am Manav Bhanot. I am learning React Native through this Udemy Course</Text>
       <StatusBar style="auto" />
     </View>
   );
